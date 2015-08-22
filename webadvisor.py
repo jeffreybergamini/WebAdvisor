da4692479fc085a31ff3c8624ad757329b3b355a
jbergamini <jbergamini@jeff.cis.cabrillo.edu>
2015-08-21 20:56:11 -0700

    Fixed delays (thanks Matera) and added add code retrieval.

--- webadvisor.py
+++ webadvisor.py
@@ -5,6 +5,9 @@ import sys
 import re
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class WebAdvisor:
   def __init__(self, username, passphrase, term):
@@ -27,15 +30,19 @@ class WebAdvisor:
     # Log in
     self.browser.get('https://wave.cabrillo.edu')
     self.browser.find_element_by_link_text('Log In').click()
+    WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "USER_NAME")))
     self.browser.find_element_by_id('USER_NAME').send_keys(self.username)
     self.browser.find_element_by_id('CURR_PWD').send_keys(self.passphrase)
     self.browser.find_element_by_id('USER_NAME').submit()
     # Faculty menu
-    self.browser.find_element_by_link_text('FACULTY: Click Here').click()
+    WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Faculty")))
+    self.browser.find_element_by_link_text('Faculty').click()
     ret = dict()
     # For each section...
     for class_num in range(1, 20):
+      WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Class Roster")))
       self.browser.find_element_by_link_text('Class Roster').click()
+      WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "VAR1")))
       Select(self.browser.find_element_by_id('VAR1')).select_by_index(1)
       self.browser.find_element_by_id('VAR1').submit()
       # Select the next section
@@ -63,11 +70,58 @@ class WebAdvisor:
           name_tokens = [x.strip().lower() for x in stu_name.split(',')]
           # My pattern for usernames is e.g. FirstInitialMiddleInitialLastName(s)
           stu_user = ''.join(x[0] for x in self.NONLETTERS_PATTERN.split(name_tokens[1]) if len(x) > 0) + ''.join(x for x in self.NONLETTERS_PATTERN.split(name_tokens[0]))
+          if len(stu_user) > 15:
+            stu_user = stu_user[:15]
           ret[class_id].append({'username': stu_user, 'name': stu_name, 'id': stu_id, 'email': stu_email, 'phone': stu_phone})
         except:
           break
       # Done with this class; return to main menu
       self.browser.find_element_by_name('SUBMIT2').submit()
+    return ret
+  def get_add_codes(self):
+    """
+    Returns a dictionary of add codes info, where keys are class ids and values are lists of add codes
+    """
+    # Log in
+    self.browser.get('https://wave.cabrillo.edu')
+    self.browser.find_element_by_link_text('Log In').click()
+    WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "USER_NAME")))
+    self.browser.find_element_by_id('USER_NAME').send_keys(self.username)
+    self.browser.find_element_by_id('CURR_PWD').send_keys(self.passphrase)
+    self.browser.find_element_by_id('USER_NAME').submit()
+    # Faculty menu
+    WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Faculty")))
+    self.browser.find_element_by_link_text('Faculty').click()
+    ret = dict()
+    # For each section...
+    for class_num in range(1, 20):
+      WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Add Codes")))
+      self.browser.find_element_by_link_text('Add Codes').click()
+      WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "VAR1")))
+      Select(self.browser.find_element_by_id('VAR1')).select_by_index(3)
+      self.browser.find_element_by_name('SUBMIT2').click()
+      # Select the next section
+      try:
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'LIST_VAR1_%d' % class_num)))
+        self.browser.find_element_by_id('LIST_VAR1_%d' % class_num).click()
+      except:
+        break
+      self.browser.find_element_by_name('SUBMIT2').click()
+      WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'VAR1')))
+      class_name = self.browser.find_element_by_id('VAR1')
+      class_tokens = [x.strip().lower() for x in class_name.text.split('-')]
+      # Take care of CS/MATH 23 crosslisting
+      if class_tokens[0] == 'math':
+        class_tokens[0] = 'cs'
+      # My pattern for class_id is DeptNumTerm
+      class_id = class_tokens[0] + class_tokens[1] + self.term
+      if class_id not in ret:
+        ret[class_id] = []
+      # All students
+      for td in self.browser.find_elements_by_class_name('LIST_VAR7'):
+        ret[class_id].append(td.text)
+      # Done with this class; return to main menu
+      self.browser.find_element_by_name('OK2').submit()
     return ret
   def __del__(self):
     self.browser.close()
