4d2349d70f7200148617902115f582cd748dc261
jbergamini <jbergamini@jeff.cis.cabrillo.edu>
2016-01-26 00:28:13 -0800

    Added better support for term names and section numbers.

--- webadvisor.py
+++ webadvisor.py
@@ -31,19 +31,35 @@ class WebAdvisor:
     self.browser.get('https://wave.cabrillo.edu')
     self.browser.find_element_by_link_text('Log In').click()
     WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "USER_NAME")))
-    self.browser.find_element_by_id('USER_NAME').send_keys(self.username)
-    self.browser.find_element_by_id('CURR_PWD').send_keys(self.passphrase)
+    user_name_field = self.browser.find_element_by_id('USER_NAME')
+    while user_name_field.get_attribute('value') != self.username:
+      user_name_field.clear()
+      self.browser.execute_script('document.getElementById("USER_NAME").value = "%s"' % self.username)
+    passphrase_field = self.browser.find_element_by_id('CURR_PWD')
+    while passphrase_field.get_attribute('value') != self.passphrase:
+      passphrase_field.clear()
+      self.browser.execute_script('document.getElementById("CURR_PWD").value = "%s"' % self.passphrase)
     self.browser.find_element_by_id('USER_NAME').submit()
     # Faculty menu
     WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Faculty")))
     self.browser.find_element_by_link_text('Faculty').click()
     ret = dict()
     # For each section...
+    full_term_name = {'s': 'Spring', 'f': 'Fall'}[self.term[0]] + ' 20' + self.term[1:]
     for class_num in range(1, 20):
       WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Class Roster")))
       self.browser.find_element_by_link_text('Class Roster').click()
       WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "VAR1")))
-      Select(self.browser.find_element_by_id('VAR1')).select_by_index(1)
+      self.browser.execute_script('''
+        document.getElementById('VAR1').selectedIndex = 1;
+        var dd = document.getElementById('VAR1');
+        for (var i = 0; i < dd.options.length; i++) {
+            if (dd.options[i].text === '%s') {
+                dd.selectedIndex = i;
+                break;
+            }
+        }
+        ''' % full_term_name)
       self.browser.find_element_by_id('VAR1').submit()
       # Select the next section
       try:
@@ -58,6 +74,7 @@ class WebAdvisor:
         class_tokens[0] = 'cs'
       # My pattern for class_id is DeptNumTerm
       class_id = class_tokens[0] + class_tokens[1] + self.term
+      section_id = class_tokens[2].split()[0]
       if class_id not in ret:
         ret[class_id] = []
       # All students
@@ -72,7 +89,7 @@ class WebAdvisor:
           stu_user = ''.join(x[0] for x in self.NONLETTERS_PATTERN.split(name_tokens[1]) if len(x) > 0) + ''.join(x for x in self.NONLETTERS_PATTERN.split(name_tokens[0]))
           if len(stu_user) > 15:
             stu_user = stu_user[:15]
-          ret[class_id].append({'username': stu_user, 'name': stu_name, 'id': stu_id, 'email': stu_email, 'phone': stu_phone})
+          ret[class_id].append({'username': stu_user, 'name': stu_name, 'id': stu_id, 'email': stu_email, 'phone': stu_phone, 'sectionid': section_id})
         except:
           break
       # Done with this class; return to main menu
@@ -86,8 +103,14 @@ class WebAdvisor:
     self.browser.get('https://wave.cabrillo.edu')
     self.browser.find_element_by_link_text('Log In').click()
     WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "USER_NAME")))
-    self.browser.find_element_by_id('USER_NAME').send_keys(self.username)
-    self.browser.find_element_by_id('CURR_PWD').send_keys(self.passphrase)
+    user_name_field = self.browser.find_element_by_id('USER_NAME')
+    while user_name_field.get_attribute('value') != self.username:
+      user_name_field.clear()
+      self.browser.execute_script('document.getElementById("USER_NAME").value = "%s"' % self.username)
+    passphrase_field = self.browser.find_element_by_id('CURR_PWD')
+    while passphrase_field.get_attribute('value') != self.passphrase:
+      passphrase_field.clear()
+      self.browser.execute_script('document.getElementById("CURR_PWD").value = "%s"' % self.passphrase)
     self.browser.find_element_by_id('USER_NAME').submit()
     # Faculty menu
     WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Faculty")))
@@ -98,7 +121,17 @@ class WebAdvisor:
       WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Add Codes")))
       self.browser.find_element_by_link_text('Add Codes').click()
       WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "VAR1")))
-      Select(self.browser.find_element_by_id('VAR1')).select_by_index(3)
+      full_term_name = {'s': 'Spring', 'f': 'Fall'}[self.term[0]] + ' 20' + self.term[1:]
+      self.browser.execute_script('''
+        document.getElementById('VAR1').selectedIndex = 1;
+        var dd = document.getElementById('VAR1');
+        for (var i = 0; i < dd.options.length; i++) {
+            if (dd.options[i].text === '%s') {
+                dd.selectedIndex = i;
+                break;
+            }
+        }
+        ''' % full_term_name)
       self.browser.find_element_by_name('SUBMIT2').click()
       # Select the next section
       try:
@@ -108,18 +141,11 @@ class WebAdvisor:
         break
       self.browser.find_element_by_name('SUBMIT2').click()
       WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'VAR1')))
-      class_name = self.browser.find_element_by_id('VAR1')
-      class_tokens = [x.strip().lower() for x in class_name.text.split('-')]
-      # Take care of CS/MATH 23 crosslisting
-      if class_tokens[0] == 'math':
-        class_tokens[0] = 'cs'
-      # My pattern for class_id is DeptNumTerm
-      class_id = class_tokens[0] + class_tokens[1] + self.term
-      if class_id not in ret:
-        ret[class_id] = []
+      class_name = self.browser.find_element_by_id('VAR1').text # Here we use the full section text as a name...
+      ret[class_name] =[]
       # All students
       for td in self.browser.find_elements_by_class_name('LIST_VAR7'):
-        ret[class_id].append(td.text)
+        ret[class_name].append(td.text)
       # Done with this class; return to main menu
       self.browser.find_element_by_name('OK2').submit()
     return ret
